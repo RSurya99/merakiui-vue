de752d8dc4688cc27937fc7bfa3fb81263aab9ca
RSurya99 <rafli.rfsp@gmail.com>
2022-03-13 14:10:17 +0700

    fix: Alert stories Canvas doesn't same with Docs

--- src/components/Alert/Alert.stories.js
+++ src/components/Alert/Alert.stories.js
@@ -1,17 +1,34 @@
 // Alert.stories.js
 
-import Alert from './Alert.vue';
+import Alert from './Alert.vue'
 
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
   title: 'Input/Alert',
   component: Alert,
-};
+}
 
-export const Default = () => ({
+const Template = (args) => ({
   components: { Alert },
-  template: '<Alert type="success" text="Hey, this is a success alert!" />',
-});
+  setup() {
+    return { args }
+  },
+  template: '<Alert v-bind="args" />',
+})
+
+export const Success = Template.bind({})
+Success.args = {
+  type: 'success',
+  title: 'Success',
+  text: 'This is a success alert message.',
+}
+
+export const Info = Template.bind({})
+Info.args = {
+  type: 'info',
+  title: 'Info',
+  text: 'This is a info alert message.',
+}
